06fa78c4987f22172f4e4059c8b50895335c4ed1
itschloenaeemah123 <owodoyin321@yahoo.com>
2025-02-13 18:48:46 +0100

    lesson3.py

---
+++ Activity2.py
@@ -0,0 +1,11 @@
+name="pingu"
+is_student= True
+age= 10
+weight= 40.5
+
+print("types: ",type(name),type(is_student),type(age),type(weight))
+#Type casting
+is_student = str(is_student)
+print(is_student,type(is_student))
+weight = int(weight)
+print(weight,type(weight))
